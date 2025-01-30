09a530e94db6ec3fbe1de5866b8663210c2c937d
root <root@DESKTOP-FHP8M5J>
2025-01-31 00:43:52 +0100

    Fix base path for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,11 @@ import About from "./components/About/About.jsx";
 import Me from './components/Me/Me.jsx';
 
 
+
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Ikigai_client/">
       <Routes>
         <Route path="/" element={<FirstScreen />} />
         <Route path="/welcome" element={<Welcome />} />
